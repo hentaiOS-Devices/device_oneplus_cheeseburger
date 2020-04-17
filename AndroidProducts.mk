5b6bc06f53b3d837d6e9cea330dcce7a68895ea3
itsjoeoui <maximeyxw@gmail.com>
2020-04-17 14:53:26 -0400

    cheeseburger: Initial hentaiOS bringup
    Signed-off-by: itsjoeoui <maximeyxw@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_cheeseburger.mk
+    $(LOCAL_DIR)/hentai_cheeseburger.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_cheeseburger-user \
-    aosp_cheeseburger-userdebug \
-    aosp_cheeseburger-eng
+    hentai_cheeseburger-user \
+    hentai_cheeseburger-userdebug \
+    hentai_cheeseburger-eng
--- hentai_cheeseburger.mk
+++ hentai_cheeseburger.mk
@@ -21,14 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cheeseburger device
 $(call inherit-product, device/oneplus/cheeseburger/device.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common hentaiOS stuff.
+$(call inherit-product, vendor/hentai/config/common_telephony.mk)
 
 TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_STOCK_ARCORE := true
 
-PRODUCT_NAME := aosp_cheeseburger
+PRODUCT_NAME := hentai_cheeseburger
 PRODUCT_DEVICE := cheeseburger
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
